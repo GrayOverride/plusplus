7fd5e1d3f982c79fe32b134161f231214dd47046
Herman Dahlén <herman.dahlen@hotmail.se>
2017-04-20 17:54:20 +0200

    get users api

---
+++ src/api/users.js
@@ -0,0 +1,22 @@
+export default class UsersApi {
+    static getUsers() {
+        fetch('http://activity.cdx.cloud/api/users')
+            .then((status) => {
+                if (response.status >= 200 && response.status < 300) {
+                    return Promise.resolve(response)
+                } else {
+                    return Promise.reject(new Error(response.statusText))
+                }
+            })
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log('Request succeded ', data);
+            }).catch((error) => {
+                console.log('Request failed ', error);
+            })
+    }
+}
+
+
